b1db2c3b4322a9deb78f716dbdfe535c2f79ea41
Vimpil <vimpil@ya.ru>
2016-04-12 11:41:01 +0300

    Jquery-ui-1.11.4_deleted

--- js/gal.js
+++ js/gal.js
@@ -1,6 +1,7 @@
  $(document).ready(function() {
  var speed = 200;
 
+     //timers
             $('#timer').timer({
                 duration: '3s',
                 callback: function () {
@@ -26,7 +27,7 @@
          });
 
 
-
+//First button
 
      $(".tab-prev").click(function() {
         var now = $(".tab-slider ul.slides").children(':visible'),
@@ -34,7 +35,6 @@
             prev = now.prev();
         prev = prev.index() == -1 ? last : prev;
         now.fadeOut(speed, function() {prev.fadeIn(speed);});
-        $('#timer').timer("remove");
     });
 
     $(".tab-next").click(function() {
@@ -44,11 +44,52 @@
         if(next.is("div")){
             next=first;
         }
-        now.fadeOut(speed,easing("easeInBounce"), function() {next.fadeIn(speed);});
-        $('#timer').timer("remove");
+        now.fadeOut(next, function() {now.fadeIn(speed);});
     });
 
 
+/*
+     var duration = 20; // duration in seconds
+     var fadeAmount = 0.3; // fade duration amount relative to the time the image is visible
+     var durationMs = duration * 1000;
+     var imageTime = durationMs / numImages; // time the image is visible
+     var fadeTime = imageTime * fadeAmount; // time for cross fading
+     var visibleTime = imageTime  - (imageTime * fadeAmount * 2);// time the image is visible with opacity == 1
+     var animDelay = visibleTime * (numImages - 1) + fadeTime * (numImages - 2); // animation delay/offset for a single image
+
+        if(index != 0){
+            $(element).css("opacity","0");
+            setTimeout(function(){
+                doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
+            },visibleTime*index + fadeTime*(index-1));
+            }else{
+            setTimeout(function(){
+            $(element).animate({opacity:0},fadeTime, function(){
+                setTimeout(function(){
+                    doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
+                },animDelay )
+            });
+        },visibleTime);
+     }
+
+// creates a animation loop
+     function doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime){
+         fadeInOut(element,fadeInTime, visibleTime, fadeOutTime ,function(){
+             setTimeout(function(){
+                 doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime);
+             },pauseTime);
+         });
+     }
+
+// shorthand for in- and out-fading
+     function fadeInOut( element, fadeIn, visible, fadeOut, onComplete){
+         return $(element).animate( {opacity:1}, fadeIn ).delay( visible ).animate( {opacity:0}, fadeOut, onComplete);
+     }
+
+*/
+
+
+//Second Button
 
 	$("div.main-right-slide.main-articles div.main-articles-buttons div.art-left").click(function() {
         var now = $(".main-articles>ul.slides").children(':visible'),
@@ -56,7 +97,6 @@
             prev = now.prev();
         prev = prev.index() == -1 ? last : prev;
         now.fadeOut(speed, function() {prev.fadeIn(speed);});
-        $('#timer2').timer("remove");
     });
 
     $("div.main-right-slide.main-articles div.main-articles-buttons div.art-right").click(function() {
@@ -67,9 +107,10 @@
             next=first;
         }
         now.fadeOut(speed, function() {next.fadeIn(speed);});
-        $('#timer2').timer("remove");
     });
 
+
+//Third Button
 
      $("div.main-right-slide.main-works div.main-articles-buttons div.art-left").click(function() {
          var now = $("div.col-xs-12 div.main-right-slide.main-works ul.slides").children(':visible'),
@@ -77,7 +118,6 @@
              prev = now.prev();
          prev = prev.index() == -1 ? last : prev;
          now.fadeOut(speed, function() {prev.fadeIn(speed);});
-         $('#timer3').timer("remove");
      });
 
      $("div.main-right-slide.main-works div.main-articles-buttons div.art-right").click(function() {
@@ -88,6 +128,5 @@
              next=first;
          }
          now.fadeOut(speed, function() {next.fadeIn(speed);});
-         $('#timer3').timer("remove");
      });
    });
