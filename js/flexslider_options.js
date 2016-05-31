0b3358bab00ef4fcf63365091f2de996e750d515
vimpil <vimpil@ya.ru>
2016-05-31 10:45:02 +0400

    made gallarys script smarter

--- js/flexslider_options.js
+++ js/flexslider_options.js
@@ -11,94 +11,25 @@ jQuery(document).ready(function () {
         }
     });
 
-
     // *index HTML*
+
         $(".tab-next").click(function(){
-            $("#"+($(this).closest("[id]").attr("id"))+" a").click();
+            $("#"+($(this).closest("[id]").attr("id"))+" a.flex-next").click();
         });
 
-/*
-        // 1.1
-        $( "#house .tab-next").click(function() {
-            $("#house a").click();
+        $(".tab-prev").click(function(){
+            $("#"+($(this).closest("[id]").attr("id"))+" a.flex-prev").click();
         });
-
-        $( "#house .tab-prev").click(function() {
-            $("#house a").click();
-        });
-*/
-        // 1.2
-
-        $( "#bath .tab-next").click(function() {
-            $("#bath a").click();
-        });
-
-        $( "#bath .tab-prev").click(function() {
-            $("#bath a").click();
-        });
-
-        // 1.3
-
-        $( "#base .tab-next").click(function() {
-            $("#base a").click();
-        });
-
-        $( "#base .tab-prev").click(function() {
-            $("#base a").click();
-        });
-
-        // 1.4
-
-        $("#kitchen .tab-next").click(function() {
-            $("#kitchen a").click();
-        });
-
-        $("#kitchen .tab-prev").click(function() {
-            $("#kitchen a").click();
-        });
-
-        // 2
-
-        $("#2st\\ slide_show .art-right").click(function() {
-            $("#2st\\ slide_show  a").click();
-        });
-
-        $("#2st\\ slide_show .art-left").click(function() {
-            $("#2st\\ slide_show a").click();
-        });
-
-        // 3
-
-        $("#3st\\ slide_show .art-right").click(function() {
-            $("#3st\\ slide_show a").click();
-
-        });
-
-        $("#3st\\ slide_show .art-left").click(function() {
-            $("#3st\\ slide_show a").click();
-        });
-
     // *elements*
 
     // 1.1
-    $( ".main-right-slide.main-articles.flexslider .art-right").click(function() {
-        $(".main-right-slide.main-articles.flexslider li.flex-nav-next a").click();
+    $( "div.art-right").click(function() {
+        $("#"+($(this).closest("[id]").attr("id"))+" li.flex-nav-next a").click();
 
     });
 
-   $( ".main-right-slide.main-articles.flexslider .art-left").click(function() {
-        $(".main-right-slide.main-articles.flexslider li.flex-nav-prev a").click();
+   $( "div.art-left").click(function() {
+       console.log("clicked");
+       $("#"+($(this).closest("[id]").attr("id"))+" li.flex-nav-prev a").click();
     });
-
-    // 1.2
-
-    $( ".main-right-slide.main-works.flexslider .art-right").click(function() {
-        $(".main-right-slide.main-works.flexslider li.flex-nav-next a").click();
-
-    });
-
-    $( ".main-right-slide.main-works.flexslider .art-left").click(function() {
-        $(".main-right-slide.main-works.flexslider li.flex-nav-prev a").click();
-    });
-
 })
